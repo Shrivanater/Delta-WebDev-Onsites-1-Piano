24284ff8069eb480cfa301ce200aa1c675fc9cab
Shrivanater <83690434+Shrivanater@users.noreply.github.com>
2021-08-24 19:25:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ blackKeys.forEach((key) => {
 let playWhiteNote = (key) => {
     var note = document.getElementById(`key${key.id}`);
     key.style.backgroundColor = "#f2f2f2"; 
+    note.currentTime = 0;
     note.play();
     window.setTimeout(function() {key.style.backgroundColor = "white";}, 600); 
 }
@@ -23,6 +24,7 @@ let playWhiteNote = (key) => {
 let playBlackNote = (key) => {
     var note = document.getElementById(`key${key.id}`);
     key.style.backgroundColor = "#333333"; 
+    note.currentTime = 0;
     note.play();
     window.setTimeout(function() {key.style.backgroundColor = "black";}, 600); 
 }
