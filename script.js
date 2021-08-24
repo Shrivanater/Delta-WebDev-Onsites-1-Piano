5eaed041e64a21e0c415221bc3e1919e1a522f6d
Shrivanater <83690434+Shrivanater@users.noreply.github.com>
2021-08-24 19:11:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let whiteKeys = document.querySelectorAll(".whiteKey");
+let blackKeys = document.querySelectorAll(".blackKey");
+
+whiteKeys.forEach((key) => {
+    key.addEventListener('click', event => {
+        playWhiteNote(key);
+    })
+});
+
+blackKeys.forEach((key) => {
+    key.addEventListener('click', event => {
+        playBlackNote(key);
+    })
+});
+
+let playWhiteNote = (key) => {
+    var note = document.getElementById(`key${key.id}`);
+    key.style.backgroundColor = "#f2f2f2"; 
+    note.play();
+    window.setTimeout(function() {key.style.backgroundColor = "white";}, 600); 
+}
+
+let playBlackNote = (key) => {
+    var note = document.getElementById(`key${key.id}`);
+    key.style.backgroundColor = "#333333"; 
+    note.play();
+    window.setTimeout(function() {key.style.backgroundColor = "black";}, 600); 
+}
